685d9d2ad65e3e3e49ca68e739ebb735759e85b3
unknown <2104481@labinfo.is.escuelaing.edu.co>
2018-10-25 17:56:16 -0500

    index

--- src/main/resources/statica/MercadoController.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=EUR&to_symbol=USD&apikey=demo"
-    }).then(function(data) {
-       $('.valores_id').append(data.id);
-       $('.valores_content').append(data.content);
-    });
-});
